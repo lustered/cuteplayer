fa66a71dbc345506ecf65adfd22e8e38826732bd
lustered <smellslikelavenders@gmail.com>
2019-11-14 20:30:21 -0500

    added button options to dl mp3/vid/both

--- mp3dl.py
+++ mp3dl.py
@@ -8,6 +8,15 @@ from pytube import YouTube as yt
 import validators as valid
 
 testing_url = "https://www.youtube.com/watch?v=-wv3ZUCy1Rg"
+download_video = "youtube-dl "
+download_mp3 = "--extract-audio --audio-format mp3 "
+# both_download = download + ' ' + mp3 + ' -k'
+
+# TODO:
+#     add support to choose either download video or mp3
+#     add soundcloud support as well
+
+    
 
 def download_url():
     url_inserted = entry.get()
@@ -29,12 +38,18 @@ def download_url():
         if exists:
             print("file exists and video won't be downloaded")
         else: 
+            # Downloading the video
             warningMessage.grid_forget()
             print('[[**** Video Downloading ****]]')
-            call(["youtube-dl --extract-audio --audio-format mp3 \
-                 %s" % (url_inserted)], shell = True)
-            print("[Song: %s \tDownloaded" % (video.title))
-    else:
+            if both_check.get():
+                call(["%s %s" % (download_video, url_inserted)], shell = True)
+                call(["%s %s %s" % (download_video, download_mp3,url_inserted)], shell = True)
+            elif mp3_check.get():
+                call(["%s %s %s" % (download_video, download_mp3, url_inserted)], shell = True)
+            else:
+                call(["%s %s" % (download_video, url_inserted)], shell = True)
+            print("[Song: %s] \t[DOWNLOADED]" % (video.title))
+    else: 
         print('** Please enter a valid URL **')
         warningMessage.grid(column = 3, row = 5)
 
@@ -48,16 +63,29 @@ window = Tk()
 
 # creating frame
 window.title('download youtube mp3')
-window.geometry('400x120')                        
+window.geometry('400x200')                        
 window.configure(background = 'lavender')
-mainlbl = Label(window,text = 'insert link', bg = 'lavender',fg = 'purple',font = ('Courier',20))
+mainlbl = Label(window,text = 'insert link', bg = 'lavender',fg = '#deafba',font = ('Courier',17))
 
 # buttons and entry setup
 download = Button(window, text ="download", bg="pink", fg = 'purple', command = download_url)
 quit = Button(window, text ="exit", bg = "pink", fg = 'purple', command = window.destroy)
 warningMessage = Label(window, bg = 'red',fg = 'black')
 warningMessage.configure(text = "Enter a valid URL")
-entry = Entry(window)
+entry = Entry(window, bg = '#f0eafe',width = 30)
+
+
+# check buttons
+video_check = IntVar()
+mp3_check = IntVar()
+both_check = IntVar() 
+
+video = Checkbutton(window, text="video", bg = 'pink', fg = 'purple', \
+        onvalue = 1, offvalue = 0, variable = video_check).grid(row = 5, column = 2)
+mp3 = Checkbutton(window, text="MP3", bg = 'pink', fg = 'purple', \
+        onvalue = 1, offvalue = 0, variable = mp3_check).grid(row=6, column = 2)
+both = Checkbutton(window, text="both", bg = 'pink', fg = 'purple', \
+        onvalue = 1, offvalue = 0, variable = both_check).grid(row=7, column = 2)
 
 # buttons,label and entry box  label placement
 mainlbl.grid(column = 3, row = 0)
