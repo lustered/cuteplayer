80a1c82f5e04292e70c0726f47cf7727db26a598
lustered <smellslikelavenders@gmail.com>
2019-11-16 13:58:43 -0500

    good enough for now

--- mp3dl.py
+++ mp3dl.py
@@ -10,63 +10,84 @@ import validators as valid
 testing_url = "https://www.youtube.com/watch?v=-wv3ZUCy1Rg"
 download_video = "youtube-dl "
 download_mp3 = "--extract-audio --audio-format mp3 "
-# both_download = download + ' ' + mp3 + ' -k'
 
 # TODO:
 #     add support to choose either download video or mp3
 #     add soundcloud support as well
 
-    
+def platform():
+    url_inserted = entry.get()
+    isvalid = False
+    plat = {'youtube': 1,'soundcloud' : 2}
+
+    if valid.url(url_inserted):
+        isvalid = True
+    else:
+        warningMessage.grid(column = 3, row = 5)
+        print('[Please enter a valid URL]')
+        return [0,0,False]  
+
+    if True:
+        platform_used = [k for k,v in plat.items() if k in url_inserted]
+        print('[Valid URL from %s]' % (platform_used[0]))
+        # check if file already exists [for YouTube only]
+        file_exists = False
+        youtube_file = None
+        if 'youtube' in platform_used:
+            youtube_file = yt(url_inserted)
+            print(youtube_file.title)
+            # terminal commands 
+            pwd = os.getcwd()
+            ls = os.listdir(pwd)
+            file_exists = False
+
+            if any(youtube_file.title in s for s in ls):
+                file_exists = True
+                print("File exists and video won't be downloaded")
+                warningMessage.configure(text = 'File already exists')
+                warningMessage.grid(column = 3, row = 3)
+            else: file_exists = False
+
+        return [platform_used[0],youtube_file,file_exists]
+
 
 def download_url():
     url_inserted = entry.get()
-    isvalid_domain = False
-    if valid.url(url_inserted) and 'youtube' in url_inserted:
-        isvalid_domain = True
-        print('[Valid URL]')
+    platform_validation = platform()
 
-    if url_inserted and isvalid_domain:
-        video = yt(url_inserted)
-        cwd = os.getcwd()
-        ls = os.listdir(cwd)
-        exists = False
-
-        for x in range(len(ls)):
-            if video.title in ls[x]:
-                exists = True
-
-        if exists:
-            print("file exists and video won't be downloaded")
-        else: 
-            # Downloading the video
-            warningMessage.grid_forget()
-            print('[[**** Video Downloading ****]]')
+    if platform_validation[2] == False:
+        # Downloading the video
+        warningMessage.grid_forget()
+        if platform_validation[0] == 'soundcloud':
+            call(["%s %s" % (download_video, url_inserted)], shell = True)
+        elif platform_validation[0] == 'youtube':
             if both_check.get():
+                # download video
                 call(["%s %s" % (download_video, url_inserted)], shell = True)
+                # download mp3
                 call(["%s %s %s" % (download_video, download_mp3,url_inserted)], shell = True)
             elif mp3_check.get():
-                call(["%s %s %s" % (download_video, download_mp3, url_inserted)], shell = True)
-            else:
+                call(["%s %s %s" % (download_video, download_mp3,url_inserted)], shell = True)
+            elif video_check.get():
                 call(["%s %s" % (download_video, url_inserted)], shell = True)
-            print("[Song: %s] \t[DOWNLOADED]" % (video.title))
-    else: 
-        print('** Please enter a valid URL **')
-        warningMessage.grid(column = 3, row = 5)
-
+            print("[Song: %s] \t[Downloaded from %s]" % (platform_validation[1].title,platform_validation[0].strip()))
+        else:
+            print('select an option')
+    else:
+        pass
 
     entry.delete(0,'end')
     entry.focus()
 
 
-# main driver
+# GUI Interface
 window = Tk()
 
 # creating frame
 window.title('download youtube mp3')
 window.geometry('400x200')                        
 window.configure(background = 'lavender')
-mainlbl = Label(window,text = 'insert link', bg = 'lavender',fg = '#deafba',font = ('Courier',17))
-
+mainlbl = Label(window,text = 'insert link', bg = 'lavender',fg = '#deafba',font = ('Courier',17)) 
 # buttons and entry setup
 download = Button(window, text ="download", bg="pink", fg = 'purple', command = download_url)
 quit = Button(window, text ="exit", bg = "pink", fg = 'purple', command = window.destroy)
