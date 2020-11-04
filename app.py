8145de6ceccd5744bafd1828fc3d1c656b825728
lustered <smellslikelavenders@gmail.com>
2020-11-04 05:28:36 -0500

    noselfupdate

--- app.py
+++ app.py
@@ -1,16 +1,8 @@
 #!/usr/bin/env python3
 from utils.interface import *
-from utils.update import update
 import sys
 
 if __name__ == "__main__":
-
-    update = os.system(" bash update.sh")
-    if update is not 0:
-        os.execv(__file__, sys.argv)
-
-    # print(os.system(" bash update.sh"))
-    #     os.execv(__file__, sys.argv)
 
     ROOT = Tk()
     ROOT.attributes('-topmost', True)
