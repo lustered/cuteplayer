1c6cf16d00fdd71cc0dfbda6e27fa73bbe38229f
lustered <smellslikelavenders@gmail.com>
2020-11-04 05:30:56 -0500

    misc

--- update.sh
+++ update.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-if ! git diff --quiet origin/master; then
-    git pull master
-    printf "Updated Cuteplayer!\n"
-    exit 1
-else
-    printf "Cuteplayer up-to-date."
-    exit 0
-fi
+# if ! git diff --quiet origin/master; then
+#     git pull origin master
+#     printf "Updated Cuteplayer!\n"
+#     exit 1
+# else
+#     printf "Cuteplayer up-to-date."
+#     exit 0
+# fi
+
+git pull origin master
