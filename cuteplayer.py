3a73158c92e714747525e29ec4458062bcb4fc06
lustered <smellslikelavenders@gmail.com>
2020-12-27 23:20:33 -0500

    Misc

--- cuteplayer.py
+++ cuteplayer.py
@@ -5,6 +5,7 @@ import sys
 if __name__ == "__main__":
 
     ROOT = Tk()
+    # Uncomment if you want the frame to stay on top
     # ROOT.attributes('-topmost', True)
     APP = Cuteplayer(master=ROOT, _theme="bliss")
     APP.mainloop()
