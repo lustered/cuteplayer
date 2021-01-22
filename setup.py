06e2aa9f9d43d385fe0e72a8fd4bb91bae557397
lustered <45863485+lustered@users.noreply.github.com>
2021-01-22 04:29:01 -0500

    Including Windows Specific .dlls and packages

--- setup.py
+++ setup.py
@@ -1,23 +1,23 @@
-#!/usr/bin/env python3.6
-import cx_Freeze
 import sys 
-import matplotlib
+import os
+import cx_Freeze
 
 base = None
 
-if sys.platform == 'win32':
-    base = 'Win32GUI'
+# TODO: Fix youtube-dl stdout
+# By hiding the console on Windows, youtube-dl won't run the process.
+#if sys.platform == 'win32':
+#    base = 'Win32GUI'
 
-options = {'build_exe':{"packages":['tkinter'],
-            'include_files':['pics','utils', 'themes']}}
-
+options = {'build_exe':{"packages":['tkinter','ffmpeg', 'ffprobe'],
+            'include_files':['DLLs', 'pics','utils', 'themes'], 
+            }}
 executables = [cx_Freeze.Executable('cuteplayer.py', base=base, icon='pics/cato.ico')]
 
 cx_Freeze.setup(name = 'cuteplayer',
         options=options,
         version='1.0',
-        author='Cluis',
+        author='https://github.com/lustered',
         description='An aesthetic retro arcade music player',
         executables=executables
-
         )
