f42813ad6da69fc9da17c6ee118bd544ba93b430
lustered <45863485+lustered@users.noreply.github.com>
2021-01-22 04:28:00 -0500

    Adding taskbar icon

--- cuteplayer.py
+++ cuteplayer.py
@@ -1,11 +1,10 @@
-#!/usr/bin/env python3
 from utils.interface import *
 import sys
 
 if __name__ == "__main__":
-
     ROOT = Tk()
-    # Uncomment if you want the frame to stay on top
-    # ROOT.attributes('-topmost', True)
+    # Stay on top
+    ROOT.attributes('-topmost', True)
+    ROOT.iconbitmap(default=r'pics/cato.ico')
     APP = Cuteplayer(master=ROOT, _theme="bliss")
     APP.mainloop()
