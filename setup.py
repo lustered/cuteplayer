2416c8af62167acffc08cf8eb542b2c0dd99bf51
lustered <smellslikelavenders@gmail.com>
2021-05-09 19:48:10 -0400

    Add tk/tcl files to path

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
 import pip
+import os
 
-# PYTHON_INSTALL_DIR = os.path.dirname(os.path.dirname(os.__file__))
-# os.environ['TCL_LIBRARY'] = os.path.join(PYTHON_INSTALL_DIR, 'tcl8.6')
-# os.environ['TK_LIBRARY'] = os.path.join(PYTHON_INSTALL_DIR, 'tk8.6')
+PYTHON_INSTALL_DIR = os.path.dirname(os.path.dirname(os.__file__))
+os.environ['TCL_LIBRARY'] = os.path.join(PYTHON_INSTALL_DIR, 'tcl8.6')
+os.environ['TK_LIBRARY'] = os.path.join(PYTHON_INSTALL_DIR, 'tk8.6')
 
 pip_packages = ["mutagen", "tk", "pygame==1.9.6", "youtube-dl", "cx_freeze"]
 pip.main(["install", *pip_packages])
